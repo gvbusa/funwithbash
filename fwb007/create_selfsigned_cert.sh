4d20b87e199485c0d1fe029fc7a2e9ee79a5ba7e
gvbusa <gvbusa@gmail.com>
2024-03-03 17:47:27 -0600

    Change domain name

--- fwb007/create_selfsigned_cert.sh
+++ fwb007/create_selfsigned_cert.sh
@@ -6,6 +6,7 @@ rm -rf etc
 mkdir -p etc/ssl/private
 mkdir -p etc/ssl/certs
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -subj "/C=US/ST=IL/L=Chicago/O=Home/CN=*.funwithbash.com" \
   -keyout etc/ssl/private/nginx-selfsigned.key \
   -out etc/ssl/certs/nginx-selfsigned.crt
 
