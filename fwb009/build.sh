37e633a3552d39029097fd6783a5c84ab4528413
gvbusa <gvbusa@gmail.com>
2024-03-10 12:19:51 -0500

    Adding version arg to build.sh

--- fwb009/build.sh
+++ fwb009/build.sh
@@ -2,6 +2,14 @@
 
 # build docker images and push them to private docker registry
 # should have first logged in to docker registry using "docker login -u <username>"
+# ./build.sh <version>
 
-docker build -t gvbusa/fwb-task-list:1.0.0 .
-docker push gvbusa/fwb-task-list:1.0.0
+# arguments
+if [[ $# -ne 1 ]]; then
+  echo "Not enough arguments. Usage: ./build.sh <version>"
+  exit 1
+fi
+VERSION=$1
+
+docker build -t gvbusa/fwb-task-list:${VERSION} .
+docker push gvbusa/fwb-task-list:${VERSION}
