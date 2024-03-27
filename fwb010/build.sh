d9c9399ce71064025337253a512f4029b60833b2
gvbusa <gvbusa@gmail.com>
2024-03-27 14:08:17 -0500

    Generalize build.sh

--- fwb010/build.sh
+++ fwb010/build.sh
@@ -2,14 +2,15 @@
 
 # build docker images and push them to private docker repository
 # should have first logged in to docker registry using "docker login -u <username>"
-# ./build.sh <version>
+# ./build.sh <repo> <version>
 
 # arguments
-if [[ $# -ne 1 ]]; then
-  echo "Not enough arguments. Usage: ./build.sh <version>"
+if [[ $# -ne 2 ]]; then
+  echo "Not enough arguments. Usage: ./build.sh <repo> <version>"
   exit 1
 fi
-VERSION=$1
+REPO=$1
+VERSION=$2
 
-docker build -t gvbusa/fwb-task-list:${VERSION} .
-docker push gvbusa/fwb-task-list:${VERSION}
+docker build -t ${REPO}:${VERSION} .
+docker push ${REPO}:${VERSION}
